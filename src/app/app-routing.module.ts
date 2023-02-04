ceadd6a662f0700c48f64cea316ab2afba027f94
taviovieira89 <taviovieira89@gmail.com>
2023-02-04 17:40:21 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
-import { NgModule } from '@angular/core';
+import { UsuarioCreateComponent } from './views/usuario/usuario-create/usuario-create.component';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { UsuarioComponent } from './views/usuario/usuario.component';
+import { ProdutoCreateComponent } from './views/produto/produto-create/produto-create.component';
+import { UsuarioLoginComponent } from './views/usuario/usuario-login/usuario-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UsuarioLoginComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "produto",
+    component: ProdutoComponent
+  },
+  {
+    path: "usuario",
+    component: UsuarioComponent
+  },
+  {
+    path: "produto/create",
+    component: ProdutoCreateComponent
+  },
+  {
+    path: "produto/create/:id",
+    component: ProdutoCreateComponent
+  },
+  {
+    path: "usuario/create",
+    component: UsuarioCreateComponent
+  },
+  {
+    path: 'usuario/create/:id',
+    component: UsuarioCreateComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,76 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { UsuarioComponent } from './views/usuario/usuario.component';
+import { MatButtonModule } from '@angular/material/button';
+import { ProdutoCreateComponent } from './views/produto/produto-create/produto-create.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule  } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProdutoReadComponent } from './views/produto/produto-read/produto-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from '@angular/common';
+import { UsuarioReadComponent } from './views/usuario/usuario-read/usuario-read.component';
+import { UsuarioCreateComponent } from './views/usuario/usuario-create/usuario-create.component';
+import { UsuarioLoginComponent } from './views/usuario/usuario-login/usuario-login.component';
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProdutoComponent,
+    UsuarioComponent,
+    ProdutoCreateComponent,
+    ProdutoReadComponent,
+    UsuarioReadComponent,
+    UsuarioCreateComponent,
+    UsuarioLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
